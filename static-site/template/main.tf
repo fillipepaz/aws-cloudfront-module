b5ef8d528b7eb2715f8750ce0c09a1c41d514c4c
Fillipe <git@bitbucket.com>
2024-04-17 00:14:46 -0300

    new template

---
+++ static-site/template/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.45.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "versioning-terraform-home"
+    key    = "site-estatico/${{ values.environment }}/${{ values.project }}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+module "cloudfront" {
+    source = "git@github.com:fillipepaz/aws-cloudfront-module.git//static-site"
+    accountId="${{ values.account }}"
+    repoNameAndOrg="${{ values.repository }}"
+    bucketName="bucket-${{ values.project }}"
+    project="${{ values.project }}" 
+    
+}
